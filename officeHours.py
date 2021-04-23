e1ac0f6995bd9281ae0fbd150f91579c7f10a0d9
eCarly <82983045+eCarly@users.noreply.github.com>
2021-04-22 17:33:04 -0700

    Slight Change
    For practice I'm doing this again with a slight change to the text.

--- officeHours.py
+++ officeHours.py
@@ -1,7 +1,7 @@
-title = "Office Hours"
+title = "The Office Hours"
 print(title)
 
-message = "Office Hours are from 9:00 am to 9:00 pm."
+message = "The Office Hours are from 9:00 am to 9:00 pm."
 print(message)
 
 portlandTime = 8
